13c42a4eb18a1396f8460a6d08aa8e8fdfe10ac0
Luciano Sabino <lb255043@gmail.com>
2025-09-22 20:11:14 -0300

    :card_file_box: Atualização do nome da univercidade

--- src/jsonInstituicoes.js
+++ src/jsonInstituicoes.js
@@ -1,13 +1,13 @@
 const instituicoes = [
-    {
-        nome: "Universidade Federal do Rio Grande do Sul",
-        sigla: "UFRGS",
-    },
+  {
+    nome: "Universidade Federal do Rio Grande",
+    sigla: "UFRG",
+  },
 
-    {
-        nome: "Universidade Federal do Recôncavo da Bahia",
-        sigla: "UFRB"
-    }
-]
+  {
+    nome: "Universidade Federal do Recôncavo da Bahia",
+    sigla: "UFRB",
+  },
+];
 
 module.exports = instituicoes;
